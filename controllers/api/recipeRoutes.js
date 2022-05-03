5dc4bcc8dcdb914e22aa1fd185ebe744c5f3cb6e
Elizabeth <ekkjohnson@gmail.com>
2022-05-03 13:40:33 -0500

    css

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -20,6 +20,12 @@ router.get('/', async (req, res) => {
         console.log(err);
     }
 })
-
+router.get('/add', async(req, res)=>{
+    try{
+        res.render('addRecipe')
+    } catch (err){
+        console.log(err);
+    }
+})
 
 module.exports = router;
