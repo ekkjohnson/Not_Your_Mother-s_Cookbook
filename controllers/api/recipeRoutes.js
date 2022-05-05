3ccfaf2095b282a462da7061181db32c2eaad062
Bret Banger <Banger484@gmail.com>
2022-05-04 20:05:02 -0500

    favorites working!

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -11,17 +11,28 @@ router.get('/', withAuth, async (req, res) => {
             attributes: ['username', 'id']
         }]
     })
-
+    const favoriteData = await Favorite.findAll()
+    const favorites = favoriteData.map((favorite) => favorite.get({ plain: true}))
+    const favoriteList = []
+    for (let i = 0; i < favorites.length; i++) {
+        if (favorites[i].user_id == req.session.user_id) {
+            const recipe = await Recipe.findByPk(favorites[i].recipe_id)
+            favoriteList.push(recipe)
+        }
+    }
+    const favs = favoriteList.map(fav => fav.get({ plain: true}))
     const recipes = recipeData.map((project) => project.get({ plain: true }))
-
+    console.log(recipes);
     try {
         res.render('recipe', {
             recipes,
+            favs
         })
     } catch (err) {
         console.log(err);
     }
 })
+
 router.get('/add', async(req, res)=>{
     try{
         res.render('addRecipe')
