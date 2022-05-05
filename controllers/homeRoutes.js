540ce76b9898280dffc8572ec02de1d40a3a553e
Elizabeth <ekkjohnson@gmail.com>
2022-05-05 11:12:13 -0500

    working fix

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 router.get('/', async (req, res) => {
     try {
         if (!req.session.logged_in) {
-            res.redirect('/api/users/login')
+            return res.redirect('/api/users/login')
         }
         res.redirect('/api/recipes')
     } catch (err) {
